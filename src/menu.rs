7dc871c257e4e4a4f7427f9e3827fb0d82f47aca
Nouha-ISEIA <nouha.bouziane@usmba.ac.ma>
2025-01-01 14:43:48 +0100

    menu.rs

--- src/menu.rs
+++ src/menu.rs
@@ -9,16 +9,16 @@ pub fn interactive_menu(budget_manager: &BudgetManager) -> Result<(), Box<dyn st
     let menu_style = Style::new().cyan().bold();
 
     loop {
-        println!("{}", menu_style.apply_to("=== Menu Interactif ==="));
-        println!("1. Ajouter un budget");
-        println!("2. Supprimer un budget");
-        println!("3. Modifier un budget");
-        println!("4. Afficher tous les budgets");
-        println!("5. Ajouter une transaction");
-        println!("6. Supprimer une transaction");
-        println!("7. Modifier une transaction");
-        println!("8. Afficher le solde restant d'un budget");
-        println!("9. Quitter");
+        println!("{}", menu_style.apply_to("=== Menu Gestionnaire Budget ==="));
+        println!("1.Ajouter un budget");
+        println!("2.Supprimer un budget");
+        println!("3.Modifier un budget");
+        println!("4.Afficher tous les budgets");
+        println!("5.Ajouter une transaction");
+        println!("6.Supprimer une transaction");
+        println!("7.Modifier une transaction");
+        println!("8.Afficher le solde restant d'un budget");
+        println!("9.Quitter\n");
         print!("{}", prompt_style.apply_to("Choisissez une option : "));
         io::stdout().flush()?;
 
@@ -43,7 +43,7 @@ pub fn interactive_menu(budget_manager: &BudgetManager) -> Result<(), Box<dyn st
                 let total_amount: f64 = match total_amount.trim().parse() {
                     Ok(val) => val,
                     Err(_) => {
-                        println!("{}", error_style.apply_to("Montant invalide. Réessayez."));
+                        println!("{}", error_style.apply_to(" Montant invalide. Réessayez "));
                         continue;
                     }
                 };
@@ -59,14 +59,14 @@ pub fn interactive_menu(budget_manager: &BudgetManager) -> Result<(), Box<dyn st
     }
             }
             "2" => {
-                print!("Nom du budget à supprimer : ");
+                print!("Nom du budget à supprimer :");
                 io::stdout().flush()?;
                 let mut budget_name = String::new();
                 io::stdin().read_line(&mut budget_name)?;
                 let budget_name = budget_name.trim();
 
                 if let Err(err) = budget_manager.remove_budget(budget_name) {
-                    eprintln!("Erreur : {}", err);
+                    eprintln!(" Erreur : {} ", err);
                 }
             }
             "3" => {
@@ -89,18 +89,18 @@ pub fn interactive_menu(budget_manager: &BudgetManager) -> Result<(), Box<dyn st
                 let new_total_amount: f64 = match new_total_amount.trim().parse() {
                     Ok(val) => val,
                     Err(_) => {
-                        println!("{}", error_style.apply_to("Montant invalide. Réessayez."));
+                        println!("{}", error_style.apply_to(" Montant invalide. Réessayez  "));
                         continue;
                     }
                 };
 
                 if let Err(err) = budget_manager.edit_budget(new_budget_name.to_string(), old_budget_name.to_string(), new_total_amount) {
-                    eprintln!("Erreur : {}", err);
+                    eprintln!(" Erreur : {}", err);
                 }
             }
             "4" => {
                 if let Err(err) = budget_manager.display_budgets() {
-                    eprintln!("Erreur : {}", err);
+                    eprintln!(" Erreur : {}", err);
                 }
             }
             "5" => {
@@ -116,20 +116,20 @@ pub fn interactive_menu(budget_manager: &BudgetManager) -> Result<(), Box<dyn st
                 io::stdin().read_line(&mut transaction_name)?;
                 let transaction_name = transaction_name.trim();
 
-                print!("Montant de la transaction : ");
+                print!("Montant de la transaction :\n ");
                 io::stdout().flush()?;
                 let mut amount = String::new();
                 io::stdin().read_line(&mut amount)?;
                 let amount: f64 = match amount.trim().parse() {
                     Ok(val) => val,
                     Err(_) => {
-                        println!("{}", error_style.apply_to("Montant invalide. Réessayez."));
+                        println!("{}", error_style.apply_to("Montant invalide. Réessayez "));
                         continue;
                     }
                 };
 
                 if let Err(err) = budget_manager.add_transaction(budget_name, transaction_name.to_string(), amount) {
-                    eprintln!("Erreur : {}", err);
+                    eprintln!(" Erreur : {}", err);
                 }
             }
             "6" => {
@@ -175,13 +175,13 @@ pub fn interactive_menu(budget_manager: &BudgetManager) -> Result<(), Box<dyn st
                 let new_amount: f64 = match new_amount.trim().parse() {
                     Ok(val) => val,
                     Err(_) => {
-                        println!("{}", error_style.apply_to("Montant invalide. Réessayez."));
+                        println!("{}", error_style.apply_to("Montant invalide. Réessayez "));
                         continue;
                     }
                 };
 
                 if let Err(err) = budget_manager.edit_transaction(budget_name, old_name.to_string(), new_name.to_string(), new_amount) {
-                    eprintln!("Erreur : {}", err);
+                    eprintln!(" Erreur : {}", err);
                 }
             }
             "8" => {
@@ -197,11 +197,11 @@ pub fn interactive_menu(budget_manager: &BudgetManager) -> Result<(), Box<dyn st
                 }
             }
             "9" => {
-                println!("{}", menu_style.apply_to("Au revoir !"));
+                println!("{}", menu_style.apply_to("Au revoir !!!"));
                 break;
             }
             _ => {
-                println!("{}", error_style.apply_to("Option invalide, veuillez réessayer."));
+                println!("{}", error_style.apply_to("Option invalide, veuillez réessayer "));
             }
         }
 
